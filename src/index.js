0343067c84fad40ac0f42c5573a67eb3816a7927
KieranVieira <kieranvieira@live.com>
2019-02-04 12:41:56 -0500

    Setup dependencys and file structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router, withRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    () => {},//reducer
+    applyMiddleware( thunk, logger )
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const AppWithRouter = withRouter(App)
+
+ReactDOM.render(
+<Provider store={store}>
+    <Router>
+        <AppWithRouter />
+    </Router>
+</Provider>
+, document.getElementById('root'));
